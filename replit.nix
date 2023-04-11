3ca28cc93df9ef27e454e25c2d2a86ca0ada5157
https123456789 <familylandon01@outlook.com>
2023-04-11 20:24:14 +0000

    feat: remove warnOnNegativeShift flag

--- replit.nix
+++ replit.nix
@@ -1,9 +1,11 @@
 { pkgs }: {
-	deps = [
-		pkgs.nasm
-  pkgs.neovim
-		pkgs.llvmPackages_9.clangUseLLVM
-		pkgs.pstree
-		pkgs.nodejs-16_x
-	];
+    deps = [
+        pkgs.openssh_with_kerberos
+        pkgs.nasm
+        pkgs.neovim
+        pkgs.llvmPackages_9.clangUseLLVM
+        pkgs.pstree
+        pkgs.nodejs-16_x
+        pkgs.git-filter-repo
+    ];
 }
